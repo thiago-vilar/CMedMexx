645e0be6d0aca679f0973eba5d9f1ce064f41dd1
thiago-vilar <oceanocabral@gmail.com>
2024-04-20 17:28:40 -0300

    commit dia 20-04-2024 C

--- frontend/src/pages/DoctorView/DoctorView.jsx
+++ frontend/src/pages/DoctorView/DoctorView.jsx
@@ -10,7 +10,71 @@ import './DoctorView.css';
 const DoctorView = () => {
     const [rooms, setRooms] = useState([]);
     const [error, setError] = useState('');
+    const [availableAppointments, setAvailableAppointments] = useState([]);
+    const [appointments, setAppointments] = useState([]);
 
+    const fetchingData = () => {
+        // Suponha que temos uma endpoint API que retorna as consultas disponíveis para agendamento
+        axios.get('http://localhost:5000/api/RoomRental')
+            .then(async response => { 
+                console.log ({response: response.data[0]});
+                setAvailableAppointments([]);
+            
+
+                await response.data.map(async d => {
+                    
+                    const userresponse = await axios.get(`http://localhost:5000/api/user/${d.userId}`)
+                    const response = await axios.get(`http://localhost:5000/api/room/${d.roomId}`)
+                    console.log(response.data);
+                    const item = {
+                        ...d,
+                        ...response.data ,
+                        id: response.data.roomId,
+                        roomRentalId: d.roomRentalId,
+                        title: response.data.hospitalName + ' - ' + userresponse.data.username, 
+                        start: response.data.start,
+                        end: response.data.end,
+                        isConfirmed: d.isConfirmed,
+
+                        
+                    };
+                    console.log({item: item});
+                
+                setAvailableAppointments(All => [...All, item]);
+                })
+
+                
+                
+            })
+            .catch(error => {
+                console.error('Error fetching available appointments:', error);
+            });
+        
+    }
+
+    useEffect(() => {fetchingData()}, []);
+    useEffect(() => {
+            axios.get('http://localhost:5000/api/Appointment')
+                    .then(async response => { 
+                        console.log ({response: response.data});
+                        setAppointments([]);
+                    
+
+                        await response.data.map(async d => {
+                            console.log ({availableAppointments})
+                            const response = availableAppointments.filter(room => room.roomRentalId == d.roomRentalId)[0]
+                         
+                            const item = {
+                                ...d,
+                            data: response,   
+                                
+                            };
+                            console.log({itemAppointiment: item});
+                        
+                        setAppointments(All => [...All, item]);
+                        })
+                    })
+    }, [availableAppointments]);
     useEffect(() => {
         fetchRooms();
     }, []);
@@ -41,7 +105,13 @@ const DoctorView = () => {
                 setError('Failed to book room.');
             });
     };
-
+    const handleEventClick = (room) => {
+        axios.patch(`http://localhost:5000/api/Appointment/confirm/${room.appointmentId}`)
+        .then(response => {
+            fetchRooms(); // Refresh the room list after booking
+            fetchingData();
+        })
+    }
     return (
         <Container className="doctor-view">
             <h2>Available Rooms</h2>
@@ -56,6 +126,7 @@ const DoctorView = () => {
                     color: room.IsBooked ? '#007bff' : '#28a745' // Blue for booked, green for available
                 }))}
             />
+            <h1>ROOMS</h1>
             <Row className="mt-3">
                 {rooms.map(room => (
                     <Col md={4} key={room.roomId}>
@@ -68,6 +139,26 @@ const DoctorView = () => {
                                     Status: {room.isBooked ? "Booked" : "Available"}
                                 </Card.Text>
                                 <Button onClick={() => bookRoom(room.roomId)} disabled={room.isBooked}>
+                                    Book Room
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            <h1>Appointments</h1>
+            <Row className="mt-3">
+                {appointments.map(room => (
+                    <Col md={4} key={room.data?.roomId}>
+                        <Card style = {{backgroundColor: room.isConfirmed? "green":"orange" }}>
+                            <Card.Body>
+                                <Card.Title>{room.data?.roomName}</Card.Title>
+                                <Card.Text>
+                                    Hospital: {room.data?.hospitalName}
+                                    <br />
+                                    Status: {room.isConfirmed ? "Booked" : "Available"}
+                                </Card.Text>
+                                <Button onClick={() => handleEventClick(room)} disabled={room.isConfirmed}>
                                     Book Room
                                 </Button>
                             </Card.Body>
--- frontend/src/pages/PatientView/PatientView.jsx
+++ frontend/src/pages/PatientView/PatientView.jsx
@@ -11,48 +11,93 @@ import './PatientView.css';
 
 const PatientView = () => {
     const [availableAppointments, setAvailableAppointments] = useState([]);
+    const [appointments, setAppointments] = useState([]);
 
-    useEffect(() => {
+    const fetchingData = () => {
         // Suponha que temos uma endpoint API que retorna as consultas disponíveis para agendamento
         axios.get('http://localhost:5000/api/RoomRental')
             .then(async response => { 
-                console.log (response.data);
-                let formattedAppointments = [];
-              
+                console.log ({response: response.data[0]});
+                setAvailableAppointments([]);
+            
 
                 await response.data.map(async d => {
                     
                     const userresponse = await axios.get(`http://localhost:5000/api/user/${d.userId}`)
                     const response = await axios.get(`http://localhost:5000/api/room/${d.roomId}`)
-                    console.log(response.data, userresponse.data);
-                    formattedAppointments.push({
+                    console.log(response.data);
+                    const item = {
+                        ...d,
                         ...response.data ,
                         id: response.data.roomId,
+                        roomRentalId: d.roomRentalId,
                         title: response.data.hospitalName + ' - ' + userresponse.data.username, 
                         start: response.data.start,
                         end: response.data.end,
+                        isConfirmed: d.isConfirmed,
+
                         
-                    });
-                console.log(formattedAppointments);
-                setAvailableAppointments(formattedAppointments);
+                    };
+                    console.log({item: item});
+                
+                setAvailableAppointments(All => [...All, item]);
                 })
+
+                
                 
             })
             .catch(error => {
                 console.error('Error fetching available appointments:', error);
             });
-    }, []);
+        
+    }
 
-    const handleEventClick = (clickInfo) => {
-        if (window.confirm(`Would you like to book an appointment with ${clickInfo.event.title}?`)) {
+    useEffect(() => {fetchingData()}, []);
+    useEffect(() => {
+            axios.get('http://localhost:5000/api/Appointment')
+                    .then(async response => { 
+                        console.log ({response: response.data});
+                        setAppointments([]);
+                    
+
+                        await response.data.map(async d => {
+                            
+                            const response = availableAppointments.filter(room => room.roomRentalId == d.roomRentalId) [0]
+                         
+                            const item = {
+                                ...d,
+                            data: response,   
+                                
+                            };
+                            console.log({itemAppointiment: item});
+                        
+                        setAppointments(All => [...All, item]);
+                        })
+                    })
+    }, [availableAppointments]);
+    const handleCalendarClick = (info) => {
+        handleEventClick (info.event);
+    };
+
+    const handleEventClick = (appointment) => {
+       
+        if (window.confirm(`Would you like to book an appointment with ${appointment.title}?`)) {
             // Aqui você pode adicionar lógica para realizar o agendamento no backend
-            axios.post('http://localhost:5000/api/appointments/book', {
-                appointmentId: clickInfo.event.id,
-                doctorId: clickInfo.event.extendedProps.doctorId
+            console.log({appointment: appointment})
+            axios.post('http://localhost:5000/api/Appointment', {
+                
+                userId: appointment.userId,
+                roomRentalId: appointment.roomRentalId,
+                start: appointment.start,
+                end: appointment.end,
+                isConfirmed: false,
             })
             .then(response => {
                 alert('Appointment booked successfully!');
+                console.log ({response: response.data});
+                axios.patch(`http://localhost:5000/api/RoomRental/confirm/`+ appointment.roomRentalId )
                 // Remover ou atualizar o evento no calendário conforme necessário
+                fetchingData();
             })
             .catch(error => {
                 console.error('Error booking appointment:', error);
@@ -68,22 +113,43 @@ const PatientView = () => {
                 plugins={[dayGridPlugin, interactionPlugin]}
                 initialView="dayGridMonth"
                 events={availableAppointments}
-                eventClick={handleEventClick}
+                eventClick={handleCalendarClick}
             />
+            <h1>Rooms</h1>
             <Row className="mt-3">
                 {availableAppointments.map(room => (
                     <Col md={4} key={room.roomId}>
-                        <Card style = {{backgroundColor: room.isBooked? "blue":"white" }}>
+                        <Card style = {{backgroundColor: room.isConfirmed? "yellow":"blue" }}>
                             <Card.Body>
                                 <Card.Title>{room.roomName}</Card.Title>
                                 <Card.Text>
                                     Hospital: {room.hospitalName}
                                     <br />
-                                    Status: {room.isBooked ? "Booked" : "Available"}
+                                    Status: {room.isConfirmed ? "Booked" : "Available"}
                                 </Card.Text>
-                                <Button onClick={() => handleEventClick(room.roomId)} disabled={room.isBooked}>
+                                <Button onClick={() => handleEventClick(room)} disabled={room.isConfirmed}>
                                     Book Room
                                 </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            <h1>Appointments</h1>
+            <Row className="mt-3">
+                {appointments.map(room => (
+                    <Col md={4} key={room.data?.roomId}>
+                        <Card style = {{backgroundColor: room.isConfirmed? "green":"orange" }}>
+                            <Card.Body>
+                                <Card.Title>{room.data?.roomName}</Card.Title>
+                                <Card.Text>
+                                    Hospital: {room.data?.hospitalName}
+                                    <br />
+                                    Status: {room.isConfirmed ? "Booked" : "Available"}
+                                    <br />
+                                    Info: {room.data?.title}
+                                </Card.Text>
+                                
                             </Card.Body>
                         </Card>
                     </Col>
